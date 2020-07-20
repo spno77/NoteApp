0625a95227073aebfc643b18aaec9471282ccbbb
spno77 <pilonoti@yahoo.com>
2020-07-20 18:16:39 +0300

    Intalled js libraries

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import axios from 'axios'
+
+import VueRouter from 'vue-router'
+
+Vue.use(axios)
+
+Vue.use(BootstrapVue)
+
+Vue.use(IconsPlugin)
+
+Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
 new Vue({
